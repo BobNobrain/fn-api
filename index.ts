869f2a1f73a3d68d0b23b95bb667f37d846480b2
BobNobrain <nikita-ivanov.mail@yandex.ru>
2018-10-03 14:16:26 +0300

    Save ts-node locally
    Epic comeback into SCPS!

--- index.ts
+++ index.ts
@@ -4,19 +4,25 @@ values(1, 2, 3)
     .if((f, s, t) => f + t > s)
         .then((f, s, t) => [f + t])
         .else((f, s, t) => [s])
-    .shrink<number>(n => n + 1)
-    .every<number>(
+    .shrink(n => n + 1)
+    .every(
         n => n + 1,
         n => n - 1,
         n => n,
         n => n ** 2
     )
     .map(n => n + 1)
-    .shrink<number>((a, b, c, d) => a + b / c - d)
+    .shrink((a, b, c, d) => a + b / c - d)
     .evalFirstAsync()
     .then(result => {
         console.log('My example', result);
     });
+
+// values(...('abc'.split('')))
+//     .map(c => `"${c}"`)
+//     .shrink((a, b, c) => [c, b, a])
+//     .evalAsync()
+//     .then(results => console.log(results));
 
 const computation = values(2, 3)
     .every(
