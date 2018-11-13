bf65d19decf7ef142c7170dadd437168abe81661
BobNobrain <nikita-ivanov.mail@yandex.ru>
2018-11-13 21:04:52 +0300

    Implement deriveAttributes for filter and zipWith

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const { plus, mulBy } = require('./lib/fns');
 
 const computationTree =
     values(1, 2, 3)
-        .if(() => Math.random() > 0.5)
-            .then(mulBy(3))
-            .else(mulBy(4))
+        .map(plus.partial(2))
+        // .if(() => Math.random() > 0.5)
+        //     .then(mulBy(3))
+        //     .else(mulBy(4))
+
+        .zipWith(
+            plus,
+            values(2, 3, 4)
+        )
+
         // .map(mulBy(2));
 
 const attributes = computationTree.deriveAttributes(ParityAttribute);
 console.log(attributes.join(', '));
 
-console.log(
-    values(-10, 10, 20, -20, 0)
-        .map(plus(10))
-        .map(mulBy(-1))
-        .deriveAttributes(SignAttribute)
-        .join(', ')
-);
+// console.log(
+//     values(-10, 10, 20, -20, 0)
+//         .map(plus.partial(10))
+//         .map(mulBy(-1))
+//         .deriveAttributes(SignAttribute)
+//         .join(', ')
+// );
