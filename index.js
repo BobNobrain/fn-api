dc26da5f7ef0838dc142e418590bdf5b0fd516a2
BobNobrain <nikita-ivanov.mail@yandex.ru>
2018-12-08 17:37:25 +0300

    Full refactor, module rearrangement
    Now it has more clean and extensible architecture

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const { values } = require('./lib/index');
 const ParityAttribute = require('./lib/attrs/ParityAttribute');
 const SignAttribute = require ('./lib/attrs/SignAttribute');
 
-const { plus, mulBy } = require('./lib/fns');
+const { plus, multiple } = require('./lib/func/arithmetics');
 
 const computationTree =
     values(1, 2, 3)
         .map(plus.partial(2))
-        // .if(() => Math.random() > 0.5)
-        //     .then(mulBy(3))
-        //     .else(mulBy(4))
+        .if(() => Math.random() > 0.5)
+            .then(multiple.partial(3))
+            .else(multiple.partial(4))
 
         .zipWith(
             plus,
             values(2, 3, 4)
         )
-        .fold(plus, 0)
+        // .fold(plus, 0)
 
         // .map(mulBy(2));
 
 const attributes = computationTree.deriveAttributes(ParityAttribute);
 console.log(attributes.join(', '));
+
+// computationTree.evalFirstAsync().then(x => console.log('Result ', x));
 
 // console.log(
 //     values(-10, 10, 20, -20, 0)
